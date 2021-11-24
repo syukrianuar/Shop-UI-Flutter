319fadfb70563d264be434e42e64b10fe3516b81
syukrianuar <2018638988@student.uitm.edu.my>
2021-11-24 16:55:40 +0800

    details page for each item

--- lib/screens/details/details_body.dart
+++ lib/screens/details/details_body.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:shopui/models/product.dart';
@@ -35,7 +35,34 @@ class DetailsBody extends StatelessWidget {
                         "this is pilah",
                         style: TextStyle(color: Colors.white),
                       ),
-                      Text("${product!.title}")
+                      Text("${product!.title}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                      Row(
+                        children: [
+                          RichText(
+                            text: TextSpan(children: [
+                              TextSpan(text: "Price\n"),
+                              TextSpan(
+                                  text: "\$${product!.price}",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline4!
+                                      .copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold))
+                            ]),
+                          ),
+                          SizedBox(width: 20,),
+                          Expanded(
+                            child: Image.asset("${product!.image}", fit: BoxFit.fitWidth,),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
