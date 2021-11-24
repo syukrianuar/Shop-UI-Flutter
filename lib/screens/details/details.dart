9ee1d7dd464378b9027be840cac8e857d8e4905b
syukrianuar <2018638988@student.uitm.edu.my>
2021-11-24 14:10:00 +0800

    add appbar on navigation item details page

--- lib/screens/details/details.dart
+++ lib/screens/details/details.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopui/models/product.dart';
 
 class DetailsScreen extends StatelessWidget {
   Product? product;
-  DetailsScreen({ Key? key, this.product}) : super(key: key);
+  DetailsScreen({Key? key, this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: product!.color,
+      appBar: AppBar(
+        backgroundColor: product!.color,
+        leading: IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/back.svg')),
+        actions: [
+          IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/searc.svg')),
+          IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/cart.svg')),
+        ],
+      ),
     );
   }
 }
-
